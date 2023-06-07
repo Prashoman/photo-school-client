bb3b8df01cfcbb013600147f5157e00f279e3945
Badhon <prashomanchakrabarti1@gmail.com>
2023-06-08 00:11:46 +0600

    update role admin and instructor api implements

--- src/Pages/Dashboard/AdminDashboard/ManageUser.jsx
+++ src/Pages/Dashboard/AdminDashboard/ManageUser.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const ManageUser = () => {
   const [axiosSecure] = useAxiosSecure();
@@ -11,6 +12,48 @@ const ManageUser = () => {
       return res.data;
     },
   });
+
+  const handleMakeInstructor = (user) => {
+    //console.log("asi");
+    fetch(`http://localhost:5000/user/instructor/${user?._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user?.email} Now instructor`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
+  const handleMakeAdmin = (user) => {
+    console.log("asi");
+    fetch(`http://localhost:5000/user/admin/${user?._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user?.email} Now Admin`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
   return (
     <div className="w-full h-full px-8">
       <div>Total User : {users.length}</div>
@@ -34,10 +77,18 @@ const ManageUser = () => {
                   <td>{user.name}</td>
                   <td>{user.email}</td>
                   <td className="flex">
-                    <button className="btn btn-sm btn-success">
+                    <button
+                      onClick={() => handleMakeInstructor(user)}
+                      disabled={user.role === "instructor"}
+                      className="btn btn-sm btn-success"
+                    >
                       Make Instructor
                     </button>
-                    <button className="btn ms-2 btn-sm btn-primary">
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      disabled={user.role === "admin"}
+                      className="btn ms-2 btn-sm btn-primary"
+                    >
                       Make Admin
                     </button>
                   </td>
